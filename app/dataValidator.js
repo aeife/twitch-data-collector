95b4a11612bc473fbe36588696ff5518ac3ab890
aeife <andre.eife@gmail.com>
2015-07-20 19:20:14 +0200

    fixed some data validator issues

--- app/dataValidator.js
+++ app/dataValidator.js
@@ -2,15 +2,24 @@ var logger = require('log4js').getLogger();
 
 module.exports = {
   validateGameData: function (data) {
-      data.forEach(function (entry) {
-        if (!entry || !entry.game) {
-          logger.error('gameData invalid');
-          logger.error(entry);
-          return false;
-        }
+      var valid = true;
+
+      if (!data.length) {
+        logger.error('gameData invalid: empty');
+        valid = false;
+      }
+
+      var missingData = data.some(function (entry) {
+        return (!entry || !entry.game);
       });
 
-      return true;
+      if (missingData) {
+        logger.error('gameData invalid: does not contain needed data');
+        logger.error(missingData);
+        valid = false;
+      }
+
+      return valid;
   },
   validateTotalStatsData: function (data) {
     return data && data.channels && data.viewers;
